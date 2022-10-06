a0e4c83876735c8514bf5cbaf8bd4ecda3746190
Jackson Aquino Primo <jacksonp@speedio.com.br>
2022-10-06 11:01:46 -0300

    add background actions

--- background.js
+++ background.js
@@ -1,14 +1,52 @@
-document.getElementById('description').innerText = "aaaaa"
+function loadInformation(url){
+  return {
+    cnpj: "9898989998998",
+    name: "carros do balacobaco"
+  }
+}
 
-chrome.tabs.onActivated.addListener(async activeInfo => {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+function setVisual(info){
+  const values = [
+    {
+      name: 'Nome',
+      field: 'name'
+    },
+    {
+      name: 'Cnpj',
+      field: 'cnpj'
+    },
+  ]
 
-  url = tab.url
+  const values_in_html = values.map((total, value)=>{
+    return total + `
+      <div>
+        ${value.name}
+        ${info[value.field]}
+      </div>
+    `
+  })
+
+}
+
+function checkIfIsInProfile(url){
   const regex = new RegExp('https://www.linkedin.com/in/');
   const found = url.match(regex);
-  if(found && found.length){
-    console.log(url)
+  return found && found.length
+}
+
+async function execute(){
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+  const url = tab.url
+  const isInProfile = checkIfIsInProfile(url)
+  if(isInProfile){
+    chrome.action.setIcon({ path: "icons/gato16.png" })
+    const information = loadInformation(url)
+    setVisual(information)
   } else {
-    console.log(url)
+    chrome.action.setIcon({ path: "icons/16.png" })
   }
-});
+}
+
+chrome.tabs.onActivated.addListener(execute);
+
+chrome.tabs.onUpdated.addListener(execute);
